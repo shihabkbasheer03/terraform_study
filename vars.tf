dc4ac3e199fe1bcdb60015b9a484d4c3a566246d
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-09 18:40:27 +0530

    vars.tf file updated

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable REGION {
+  default = "us-east-1"
+}
+
+variable ZONE1 {
+default = "us-east-1a"
+
+}
+
+variable AMIS {
+  type = map
+  default{
+    us-east-1 = "ami-063d43db0594b521b"
+    us-east-2 = "ami-0fae88c1e6794aa17"
+  }
+
+}
