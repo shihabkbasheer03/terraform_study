b46228561f6c116e8fed6d7b6e9a07ef0a88af6d
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-09 19:02:12 +0530

    instance.tf file updated

--- instances.tf
+++ instances.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "dove-inst" {
     ami = var.AMIS[var.REGION]
+    instance_type = "t2.micro"
     availability_zone = var.ZONE1
     key_name = "dove-key"
     vpc_security_group_ids = ["sg-0cf73c9432178f577"]
