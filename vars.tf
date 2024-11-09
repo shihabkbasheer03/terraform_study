d54ea33a4a466431bbaec87355a626874d337308
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-09 18:54:26 +0530

    vars edited

--- vars.tf
+++ vars.tf
@@ -1,17 +1,15 @@
-variable REGION {
+variable "REGION" {
   default = "us-east-1"
 }
 
-variable ZONE1 {
-default = "us-east-1a"
-
+variable "ZONE1" {
+  default = "us-east-1a"
 }
 
-variable AMIS {
+variable "AMIS" {
   type = map
-  default{
+  default = {
     us-east-1 = "ami-063d43db0594b521b"
     us-east-2 = "ami-0fae88c1e6794aa17"
   }
-
 }
