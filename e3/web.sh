968e236e3fbdc7ba0dea337823696fd9d38a2d99
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-12 15:39:55 +0530

    updated

--- e3/web.sh
+++ e3/web.sh
@@ -2,7 +2,7 @@
 sudo yum update -y
 sudo yum install -y wget unzip httpd
 sudo systemctl start httpd
-sudo systemctl enable httpd
+    sudo systemctl enable httpd
 wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
 unzip -o 2117_infinite_loop.zip
 cp -r 2117_infinite_loop/* /var/www/html
