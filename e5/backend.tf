3faafb22868f42ab94e665cf3d760232fc4b72eb
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-12 19:46:19 +0530

    backenf file updated

--- e5/backend.tf
+++ e5/backend.tf
@@ -1,8 +1,7 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-dove-state311"
-        key = "terraform/backend"
-        region = "us-east-1"
+        bucket = "terraform-dove-state311"      # S3 bucket name
+        key    = "terraform/backend"            # Path within the bucket
+        region = "us-east-1"                    # Region where the bucket is located
     }
-
 }
