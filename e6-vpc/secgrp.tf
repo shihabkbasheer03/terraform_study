342882a72ec23ce71d15dd871f18d2ebb4e1966c
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 08:07:54 +0530

    myIP added for SSH

--- e6-vpc/secgrp.tf
+++ e6-vpc/secgrp.tf
@@ -21,5 +21,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+  cidr_blocks      = [var.MYIP]
 
 }
