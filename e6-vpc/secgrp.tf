7972cb56052a9971e72bd26fc5a3ee9e301711c0
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 08:12:38 +0530

    cidr updated

--- e6-vpc/secgrp.tf
+++ e6-vpc/secgrp.tf
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.dove_stack_sg.id
-  cidr_ipv4         = aws_vpc.dove.cidr_block
+  #cidr_ipv4         = aws_vpc.dove.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
