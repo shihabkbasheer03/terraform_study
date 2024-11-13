6df4e00315607c1666d975982ed2a083011370b0
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 15:13:45 +0530

    17 line cidr_blocks

--- e7-Cloud_state_terraform/secgrp.tf
+++ e7-Cloud_state_terraform/secgrp.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "vprofile-bean-elb-sg" {
     from_port         = 80
     to_port           = 80
     protocol          = "tcp"
-    cidr_blocks       = [0.0.0.0/0]
+    cidr_blocks       = ["0.0.0.0/0"]
   }
 
 }
