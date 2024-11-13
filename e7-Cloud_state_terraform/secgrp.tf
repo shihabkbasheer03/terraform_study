bd1e3b4c17eddda279bce01752913844678af9ba
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 15:11:13 +0530

    secgroup updated

---
+++ e7-Cloud_state_terraform/secgrp.tf
@@ -0,0 +1,90 @@
+resource "aws_security_group" "vprofile-bean-elb-sg" {
+  name        = "vprofile-bean-elb-sg"
+  description = "Security group for bean-elb"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    cidr_blocks       = [0.0.0.0/0]
+  }
+
+}
+
+resource "aws_security_group" "vprofile-bastin-sg" {
+  name        = "vprofile-bastin-sg"
+  description = "Security group for bastionisioner ec2 instances"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = [var.MYIP]
+  }
+}
+
+resource "aws_security_group" "vprofile-prod-sg" {
+  name        = "vprofile-prod-sg"
+  description = "Security group for beanstalk instances"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    Security_groups = [aws_security_group.vprofile-bastin-sg.id]
+
+  }
+
+}
+
+resource "aws_security_group" "vprofile-backend-sg" {
+  name        = "vprofile-backend-sg"
+  description = "Security group for RDS, ACTIVEMQ, Elastic Cache instances"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    Security_groups = [aws_security_group.vprofile-prod-sg.id]
+
+  }
+}
+
+resource "aws_security_group" "sec_group_allow_itself" {
+  type = "ingress"
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  security_group_id = aws_security_group.vprofile-backend-sg.id
+  source_security_group_id = aws_security_group.vprofile-backend-sg.id
+}
