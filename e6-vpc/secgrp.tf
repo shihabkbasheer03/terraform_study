a45cf0fe3bd36d54a2db51943ce5f3c39883407a
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 08:23:31 +0530

    sg file updated

--- e6-vpc/secgrp.tf
+++ e6-vpc/secgrp.tf
@@ -3,23 +3,21 @@ resource "aws_security_group" "dove_stack_sg" {
   description = "Sec Grp for dove ssh"
   vpc_id      = aws_vpc.dove.id
 
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = [var.MYIP]
+  }
+
   tags = {
     Name = "allow_ssh"
   }
-}
-
-resource "aws_security_group_rule" "allow_all_traffic_egress" {
-  type              = "egress"
-  security_group_id = aws_security_group.dove_stack_sg.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  ip_protocol       = "-1"  # Allows all protocols and ports
-}
-
-resource "aws_security_group_rule" "allow_ssh_ingress" {
-  type              = "ingress"
-  security_group_id = aws_security_group.dove_stack_sg.id
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [var.MYIP]
 }
