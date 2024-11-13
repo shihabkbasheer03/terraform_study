1a93d7336658027c7198285d4548f8c97a1e5971
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 10:45:17 +0530

    instance.tf file corrected

--- e6-vpc/instances.tf
+++ e6-vpc/instances.tf
@@ -1,19 +1,37 @@
 resource "aws_key_pair" "dove-key" {
-    key_name = "dovekey"
-    public_key = file(var.PUB_KEY)
-
+  key_name   = "dovekey"
+  public_key = file(var.PUB_KEY)
 }
 
 resource "aws_instance" "dove-dove" {
-    ami = var.AMIS[var.REGION]
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.dove-pub-1.id
-    key_name = aws_key_pair.dove-key.key_name
-    vpc_security_group_ids = [aws_security_group.dove_stack_sg.id]
-    tags = {
-        Name = "my-dove"
-    }
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.dove-pub-1.id
+  key_name               = aws_key_pair.dove-key.key_name
+  vpc_security_group_ids = [aws_security_group.dove_stack_sg.id]
+  tags = {
+    Name = "my-dove"
+  }
+
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
+
+  connection {
+    user        = var.USER
+    private_key = file("dovekey")
+    host        = self.public_ip
+  }
 }
+
 resource "aws_ebs_volume" "vol_4_dove" {
   availability_zone = var.ZONE1
   size              = 3
@@ -28,30 +46,10 @@ resource "aws_volume_attachment" "atch_vol_dove" {
   instance_id = aws_instance.dove-dove.id
 }
 
-    provisioner "file" {
-        source = "web.sh"
-        destination = "/tmp/web.sh"
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "chmod u+x /tmp/web.sh",
-            "sudo /tmp/web.sh"
-        ]
-
-    }
-    connection {
-        user = var.USER
-        private_key = file("dovekey")
-        host = self.public_ip
-    }
-
-
 output "PrivateIP" {
-    value = aws_instance.dove-dove.private_ip
-
+  value = aws_instance.dove-dove.private_ip
 }
 
 output "publicIP" {
-    value = aws_instance.dove-dove.public_ip
-
+  value = aws_instance.dove-dove.public_ip
 }
