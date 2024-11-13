13e8b156e68df80e00c30c2016eed4df8e4c5b9c
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 08:04:03 +0530

    error corrected

--- e6-vpc/secgrp.tf
+++ e6-vpc/secgrp.tf
@@ -3,10 +3,14 @@ resource "aws_security_group" "dove_stack_sg" {
   description = "Sec Grp for dove ssh"
   vpc_id      = aws_vpc.dove.id
 
+  tags = {
+    Name = "allow_ssh"
+  }
 
+}
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.dove_stack_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }  
@@ -17,9 +21,5 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
-}
 
-  tags = {
-    Name = "allow_ssh"
-  }
 }
