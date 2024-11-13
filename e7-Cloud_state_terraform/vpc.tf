e2ed3d01d10337dd8caeb12fb9864fac59644c2d
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-13 14:13:24 +0530

    vpc module added

---
+++ e7-Cloud_state_terraform/vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+    source = "terraform-aws-module/vpc/aws"
+
+    name = var.VPC_NAME
+    cidr = var.VpcCIDR
+    azs = [var.ZONE1, var.ZONE2, var.ZONE3]
+    private_subnets = [var.PriSub1CIDR, var.PriSub2CIDR, var.PriSub3CIDR]
+    public_subnets = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        Terraform = "true"
+        Environment = "Prod"
+    }
+    vac_tags {
+        name = var.VPC_NAME
+    }
+}
+
